656b005168850bf5c6e6b303e7def6f581bb6862
Oleksandr Toropov <toropov@gmail.com>
2025-07-06 00:51:29 +0300

    Delete shs directory

--- shs/1-stream.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-ffmpeg -re -i "rtmp://127.0.0.1:1935/onlinestage/test" -c copy -f flv "rtmps://live-api-s.facebook.com:443/rtmp/FB-10223262012577642-0-Ab0ZI15rPLwPL8MTqgErJ3ix" -ignore_unknown -shortest
--- shs/2-stream.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-ffmpeg -re -i "rtmp://127.0.0.1:1935/onlinestage/test" -c copy -f flv "rtmp://a.rtmp.youtube.com/live2/abhk-7rfx-2v7y-hrh2-3jcw" -ignore_unknown -shortest
