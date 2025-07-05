e3e129edb9225e8d2049c3dfb0ecf44aefabdeab
Oleksandr Toropov <toropov@gmail.com>
2025-07-06 00:27:22 +0300

    Update git.sh

--- git.sh
+++ git.sh
@@ -3,5 +3,4 @@ git clone https://github.com/yatoropov/stagegrid.git
 cd stagegrid
 chmod +x install.sh
 chmod +x start.sh
-chmod +x shs/2-stream.sh
-chmod +x shs/1-stream.sh
+chmod +x generate_restreams.sh
