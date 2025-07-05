bc95b85275603033721d69b91f8bc1174a21a6e1
Oleksandr Toropov <toropov@gmail.com>
2025-07-05 22:22:08 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -5,8 +5,8 @@ sudo add-apt-repository -y ppa:mc3man/trusty-media
 sudo apt-get install -y ffmpeg
 sudo apt install -y nginx libnginx-mod-rtmp
 sudo systemctl restart nginx
-sudo apt install net-tools
-sudo apt install unzip
+sudo apt install -y net-tools
+sudo apt install -y unzip
 
 fileid=18uEA4rpfmOPjqOBLwuaKelaMRWa9MGn3
 curl -L "https://drive.usercontent.google.com/download?id=${fileid}&export=download&confirm=t" -o restream.zip
